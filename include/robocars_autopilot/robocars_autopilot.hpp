2684f76451cf8f18dbc2edaa0aa86bcd199c1f5b
Benoit TRINITE <btrinite@free.fr>
2020-04-03 21:08:12 +0200

    first commit

---
+++ include/robocars_autopilot/robocars_autopilot.hpp
@@ -0,0 +1,103 @@
+#include <tinyfsm.hpp>
+#include <ros/ros.h>
+#include <stdio.h>
+
+
+struct BaseEvent : tinyfsm::Event
+{
+    public:
+        BaseEvent(const char * evtName) : _evtName(evtName) {};
+        const char * getEvtName() const { return _evtName; };
+    private:
+        const char *  _evtName;
+};
+
+struct TickEvent                    : BaseEvent { public: TickEvent() : BaseEvent("TickEvent") {}; };
+struct IdleStatusEvent              : BaseEvent { public: IdleStatusEvent() : BaseEvent("IdleStatusEvent") {}; };
+struct ManualDrivingEvent           : BaseEvent { public: ManualDrivingEvent() : BaseEvent("ManualDrivingEvent") {}; };
+struct AutonomousDrivingEvent       : BaseEvent { public: AutonomousDrivingEvent() : BaseEvent("AutonomousDrivingEvent") {}; };
+
+class RobocarsStateMachine
+: public tinyfsm::Fsm<RobocarsStateMachine>
+{
+    public:
+        RobocarsStateMachine(const char * stateName) : _stateName(stateName), tinyfsm::Fsm<RobocarsStateMachine>::Fsm() { 
+            ROS_INFO("Autopilot StateMachine: State created: %s", _stateName);      
+        };
+        const char * getStateName() const { return _stateName; };
+
+    public:
+        /* default reaction for unhandled events */
+        void react(BaseEvent const & ev) { 
+            ROS_INFO("state %s: unexpected event %s reveived", getStateName(), ev.getEvtName());      
+        };
+
+        virtual void react(TickEvent                      const & e) { /*logEvent(e);*/ };
+        virtual void react(IdleStatusEvent                const & e) { logEvent(e); };
+        virtual void react(ManualDrivingEvent             const & e) { logEvent(e); };
+        virtual void react(AutonomousDrivingEvent         const & e) { logEvent(e); };
+
+        virtual void entry(void) { 
+            ROS_INFO("State %s: entering", getStateName()); 
+        };  
+        void         exit(void)  { };  /* no exit actions */
+
+    private:
+        const char *  _stateName ="NoName";
+        void logEvent(BaseEvent const & e) {
+            ROS_INFO("State %s: event %s", getStateName(), e.getEvtName());
+        }
+};
+
+typedef tinyfsm::FsmList<RobocarsStateMachine> fsm_list;
+
+template<typename E>
+void send_event(E const & event)
+{
+  fsm_list::template dispatch<E>(event);
+}
+
+
+
+class RosInterface
+{
+    public :
+        RosInterface() : node_("~") {
+            initParam();
+            updateParam();
+            it = new image_transport::ImageTransport(node_);
+            tensorflow::SessionOptions options = tensorflow::SessionOptions();
+            options.config.mutable_gpu_options()->set_allow_growth(true);
+            tensorflow::Status status = tensorflow::NewSession(options, &session);
+            if (!status.ok()) {
+
+            } else {
+                ROS_INFO("Autopilot: TF Session initialized");
+            }
+        };
+
+
+        void initParam();
+        void updateParam();
+        void initSub();
+
+    private:
+
+        void state_msg_cb(const robocars_msgs::robocars_brain_state::ConstPtr& msg);
+
+        ros::NodeHandle node_;
+        ros::Subscriber state_sub;
+
+        void callbackWithCameraInfo(const sensor_msgs::ImageConstPtr& image_msg, const sensor_msgs::CameraInfoConstPtr& info);
+        void tof1_msg_cb(const robocars_msgs::robocars_tof::ConstPtr& msg);
+        void tof2_msg_cb(const robocars_msgs::robocars_tof::ConstPtr& msg);
+
+        ros::Subscriber tof1_sub;
+        ros::Subscriber tof2_sub;
+        image_transport::ImageTransport * it;
+        image_transport::CameraSubscriber sub_image_and_camera;
+
+        tensorflow::Session* session;
+        tensorflow::GraphDef graph_def;
+};
+
---
+++ src/robocars_autopilot.cpp
@@ -0,0 +1,246 @@
+/**
+ * @file offb_raw_node.cpp
+ * @brief Offboard control example node, written with MAVROS version 0.19.x, PX4 Pro Flight
+ * Stack and tested in Gazebo SITL
+ source $src_path/Tools/setup_gazebo.bash ${src_path} ${build_path}
+
+ gzserver --verbose ${src_path}/Tools/sitl_gazebo/worlds/${model}.world &
+ */
+#include <tinyfsm.hpp>
+#include <ros/ros.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fstream>
+
+#include <boost/format.hpp>
+
+#include <date.h>
+#include <json.hpp>
+
+#include "tensorflow/core/public/session.h"
+#include "tensorflow/core/platform/env.h"
+
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+
+#include <image_transport/image_transport.h>
+
+
+#include <robocars_msgs/robocars_brain_state.h>
+#include <robocars_msgs/robocars_tof.h>
+
+#include <robocars_autopilot.hpp>
+
+RosInterface * ri;
+
+static int loop_hz;
+static std::string model_filename;
+
+class onRunningMode;
+class onIdle;
+class onManualDriving;
+class onAutonomousDriving;
+
+class onRunningMode
+: public RobocarsStateMachine
+{
+    public:
+        onRunningMode() : RobocarsStateMachine("onRunningMode"),__tick_count(0) {};
+        onRunningMode(const char * subStateName) : RobocarsStateMachine(subStateName),__tick_count(0) {};
+
+
+    protected:
+
+        uint32_t __tick_count;
+        
+        void entry(void) override {
+            RobocarsStateMachine::entry();
+        };
+
+        void react(ManualDrivingEvent const & e) override { 
+            RobocarsStateMachine::react(e);
+        };
+
+        void react( AutonomousDrivingEvent const & e) override { 
+            RobocarsStateMachine::react(e);
+        };
+
+        void react( IdleStatusEvent const & e) override { 
+            RobocarsStateMachine::react(e);
+        };
+
+        void react(TickEvent const & e) override {
+        };
+
+};
+
+class onIdle
+: public onRunningMode
+{
+    public:
+        onIdle() : onRunningMode("onArm") {};
+
+    private:
+
+        void entry(void) override {
+            onRunningMode::entry();
+        };
+  
+        void react(ManualDrivingEvent const & e) override { 
+            onRunningMode::react(e);
+            transit<onManualDriving>();
+        };
+
+
+        void react(TickEvent const & e) override {
+            onRunningMode::react(e);
+        };
+
+};
+
+class onManualDriving
+: public onRunningMode
+{
+    public:
+        onManualDriving() : onRunningMode("onManualDriving") {};
+
+    private:
+
+        void entry(void) override {
+            onRunningMode::entry();
+        };
+
+        void react (AutonomousDrivingEvent const & e) override {
+            onRunningMode::react(e);
+            transit<onAutonomousDriving>();
+        }
+
+        void react(IdleStatusEvent const & e) override { 
+            onRunningMode::react(e);
+            transit<onIdle>();
+        };
+
+        void react (TickEvent const & e) override {
+            onRunningMode::react(e);
+        };
+
+};
+
+class onAutonomousDriving
+: public onRunningMode
+{
+    public:
+        onAutonomousDriving() : onRunningMode("onAutonomousDriving") {};
+
+    private:
+
+        virtual void entry(void) { 
+            onRunningMode::entry();
+        };  
+
+        virtual void react(IdleStatusEvent                 const & e) override { 
+            onRunningMode::react(e);
+            transit<onIdle>();
+        };
+
+        virtual void react(ManualDrivingEvent              const & e) override { 
+            onRunningMode::react(e);
+            transit<onManualDriving>();
+        };
+
+        virtual void react(TickEvent                      const & e) override { 
+            onRunningMode::react(e);
+        };
+};
+
+FSM_INITIAL_STATE(RobocarsStateMachine, onIdle)
+
+uint32_t mapRange(uint32_t in1,uint32_t in2,uint32_t out1,uint32_t out2,uint32_t value)
+{
+  if (value<in1) {value=in1;}
+  if (value>in2) {value=in2;}
+  return out1 + ((value-in1)*(out2-out1))/(in2-in1);
+}
+
+void RosInterface::initParam() {
+    if (!node_.hasParam("loop_hz")) {
+        node_.setParam ("loop_hz", 30);       
+    }
+    if (!node_.hasParam("model_filename")) {
+        node_.setParam("model_filename",  std::string("modelcat"));
+    }
+}
+void RosInterface::updateParam() {
+    node_.getParam("loop_hz", loop_hz);
+    node_.getParam("model_filename", model_filename);
+}
+
+
+void RosInterface::initSub () {
+    sub_image_and_camera = it->subscribeCamera("/front_video_resize/image", 1, &RosInterface::callbackWithCameraInfo, this);
+    tof1_sub = node_.subscribe<robocars_msgs::robocars_tof>("/sensors/tof1", 1, &RosInterface::tof1_msg_cb, this);
+    tof2_sub = node_.subscribe<robocars_msgs::robocars_tof>("/sensors/tof2", 1, &RosInterface::tof2_msg_cb, this);
+    state_sub = node_.subscribe<robocars_msgs::robocars_brain_state>("/robocars_brain_state", 1, &RosInterface::state_msg_cb, this);
+}
+
+static uint32_t lastTof1Value;
+static uint32_t lastTof2Value;
+
+void RosInterface::callbackWithCameraInfo(const sensor_msgs::ImageConstPtr& image_msg, const sensor_msgs::CameraInfoConstPtr& info) {
+
+}
+
+void RosInterface::tof1_msg_cb(const robocars_msgs::robocars_tof::ConstPtr& msg){
+    lastTof1Value = msg->distance;
+}
+
+void RosInterface::tof2_msg_cb(const robocars_msgs::robocars_tof::ConstPtr& msg){
+    lastTof2Value = msg->distance;
+}
+
+void RosInterface::state_msg_cb(const robocars_msgs::robocars_brain_state::ConstPtr& msg) {    
+    static u_int32_t last_state = -1;
+    if (msg->state != last_state) {
+        switch (msg->state) {
+            case robocars_msgs::robocars_brain_state::BRAIN_STATE_IDLE:
+                send_event(IdleStatusEvent());        
+            break;
+            case robocars_msgs::robocars_brain_state::BRAIN_STATE_MANUAL_DRIVING:
+                send_event(ManualDrivingEvent());        
+            break;
+            case robocars_msgs::robocars_brain_state::BRAIN_STATE_AUTONOMOUS_DRIVING:
+                send_event(AutonomousDrivingEvent());        
+            break;
+        }
+        last_state=msg->state;
+    }    
+}
+
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "robocars_autopilot");
+    
+    ri = new RosInterface;
+
+    fsm_list::start();
+    ri->initSub();
+
+    ROS_INFO("Autopilot: Starting");
+
+    // wait for FCU connection
+    ros::Rate rate(loop_hz);
+    while(ros::ok()){
+        ros::spinOnce();
+        send_event (TickEvent());
+        rate.sleep();
+    }
+}
+
