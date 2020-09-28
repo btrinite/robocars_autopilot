baa0e1e357fb19c9e218d2968a9c393805738c2a
Benoit TRINITE <btrinite@free.fr>
2020-09-28 13:57:21 +0200

    fix

--- src/robocars_autopilot.cpp
+++ src/robocars_autopilot.cpp
@@ -559,11 +559,11 @@ void RosInterface::callbackNoCameraInfo(const sensor_msgs::ImageConstPtr& image_
         } else {
             if (autobrake_enabled == true) {
                 //Model do not provide brake, implement basic logic
-                    ROS_INFO("Autopilot : autobrake: steering %f speed %f", predicted_Steering, lastSpeedValue);
+                    ROS_INFO("Autopilot : autobrake: steering %d speed %d", predicted_Steering, lastSpeedValue);
                 if (fabs(predicted_Steering)> autobrake_steering_thresh) {
                     if (lastSpeedValue>autobrake_speed_thresh) {
                         predicted_Brake = 0.0 - (fmapRange (autobrake_speed_thresh,autobrake_speed_max,0,1,lastSpeedValue) * autobrake_brake_factor);
-                        ROS_INFO("Autopilot : apply brake: %f", predicted_Brake);
+                        ROS_INFO("Autopilot : apply brake: %d", predicted_Brake);
                     }
                 }
             }
